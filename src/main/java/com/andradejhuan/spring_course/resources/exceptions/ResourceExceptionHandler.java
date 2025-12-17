2e68d4d08531c73f917d28894b9da9bb03fa3a55
Jhuan <jhuanconta@gmail.com>
2025-12-17 19:13:38 -0300

    Exception handling - delete

--- src/main/java/com/andradejhuan/spring_course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/andradejhuan/spring_course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.andradejhuan.spring_course.resources.exceptions;
 
+import com.andradejhuan.spring_course.services.exceptions.DatabaseException;
 import com.andradejhuan.spring_course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -20,4 +21,11 @@ public class ResourceExceptionHandler extends RuntimeException {
     return ResponseEntity.status(status).body(err);
   }
 
+  @ExceptionHandler(DatabaseException.class)
+  public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+    String error = "Database error";
+    HttpStatus status = HttpStatus.BAD_REQUEST;
+    StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+    return ResponseEntity.status(status).body(err);
+  }
 }
--- src/main/java/com/andradejhuan/spring_course/services/UserService.java
+++ src/main/java/com/andradejhuan/spring_course/services/UserService.java
@@ -2,8 +2,11 @@ package com.andradejhuan.spring_course.services;
 
 import com.andradejhuan.spring_course.entities.User;
 import com.andradejhuan.spring_course.repositories.UserRepository;
+import com.andradejhuan.spring_course.services.exceptions.DatabaseException;
 import com.andradejhuan.spring_course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -30,7 +33,13 @@ public class UserService {
     }
 
     public void delete(Long id){
+        try {
         repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
